668ad5d1e27228898c5cc33f3baf23bb9d58381b
eddanieles <ed.danieles@yahoo.com>
2016-08-03 19:47:33 -0400

    updated files

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(".submenu").slideUp();
 
 $(".heading").click(function(){
   $(".submenu").hide();
-  $(this).next(".submenu").slideToggle();
+  $(this).next(".submenu").slideToggle(1000);
 
   $(".heading").removeClass("toggleOn");
   $(this).toggleClass("toggleOn");
